4c4512507b724c7350ef78efa1243c4bd97df12f
gianverdum <gian.verdum@gmail.com>
2025-02-20 12:47:37 -0300

    Created deleteFile

--- video-processing-service/src/storage.ts
+++ video-processing-service/src/storage.ts
@@ -75,3 +75,26 @@ export async function uploadProcessedVideo(fileName: string) {
 
     await bucket.file(fileName).makePublic();
 }
+
+/**
+ * @param filePath - The path of the file to delete.
+ * @returns A promise that resolves when the file has been deleted.
+ */
+function deleteFile(filePath: string): Promise<void> {
+    return new Promise((resolve, reject) => {
+        if (fs.existsSync(filePath)) {
+            fs.unlink(filePath, (err) => {
+                if (err) {
+                    console.log(`Failed to delete file at ${filePath}.`, err);
+                    reject(err);
+                } else {
+                    console.log(`File deleted at ${filePath}.`);
+                    resolve();
+                }
+            })
+        } else {
+            console.log(`File not found at ${filePath}, skipping deletion.`);
+            resolve();
+        }
+    });
+}
