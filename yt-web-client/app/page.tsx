5f97395ae7e14e4e5e59d9ab0afe4d49c84d333d
gianverdum <gian.verdum@gmail.com>
2025-02-21 12:10:38 -0300

    Creating yt-web-client project structure with next.js

---
+++ yt-web-client/app/page.tsx
@@ -0,0 +1,15 @@
+import styles from './page.module.css'
+
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <p>
+          Get Started by editing&nbsp;
+          <code className={styles.code}>pages/index.tsx</code>
+        </p>
+      </div>
+    </main>
+  )
+}
