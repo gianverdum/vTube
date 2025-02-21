424086f06191b098fc89b580bc4886f235abfc6f
gianverdum <gian.verdum@gmail.com>
2025-02-21 16:51:59 -0300

    Added comments to firebase.ts

--- yt-web-client/app/firebase/firebase.ts
+++ yt-web-client/app/firebase/firebase.ts
@@ -17,16 +17,29 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 const auth = getAuth(app);
 
+/**
+ * Signs the user in with a Google popup.
+ * @returns A promise that resolves with the user's credentials.
+ */
 export function signInWithGoogle() {
-    return signInWithPopup(auth, new GoogleAuthProvider());
+  return signInWithPopup(auth, new GoogleAuthProvider());
 }
 
+/**
+ * Signs the user out.
+ * @returns A promise that resolves when the user is signed out.
+ */
 export function signOut() {
-    return auth.signOut();
+  return auth.signOut();
 }
 
+/**
+ * Trigger a callback when user auth state changes.
+ * @returns A function to unsubscribe callback.
+ */
 export function onAuthStateChangedHelper(callback: (user: User | null) => void) {
-    return onAuthStateChanged(auth, callback);
+  return onAuthStateChanged(auth, callback);
 }
