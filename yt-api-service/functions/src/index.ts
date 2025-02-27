f426a154f2eacd6491eaf102f54aaa43eaf0f302
gianverdum <gian.verdum@gmail.com>
2025-02-27 15:33:05 -0300

    Created getVideos cloud function

--- yt-api-service/functions/src/index.ts
+++ yt-api-service/functions/src/index.ts
@@ -22,8 +22,9 @@ initializeApp();
 
 const firestore = new Firestore();
 const storage = new Storage();
-
 const rawVideoBucketName = "vtube-raw-videos";
+
+const videoCollectionId = "videos";
 
 export const createUser = functions.auth.user().onCreate((user) => {
   const userInfo = {
@@ -63,3 +64,9 @@ export const generateUploadUrl = onCall(
 
     return {url, fileName};
   });
+
+export const getVideos = onCall({maxInstances: 1}, async () => {
+  const snapshot = await firestore
+    .collection(videoCollectionId).limit(10).get();
+  return snapshot.docs.map((doc) => doc.data());
+});
