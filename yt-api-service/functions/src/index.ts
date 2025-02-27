f2c990df903881d5c872719a9d4c76ac5f42395d
gianverdum <gian.verdum@gmail.com>
2025-02-27 10:41:14 -0300

    Created generateUploadUrl cloud function and set cors

--- yt-api-service/functions/src/index.ts
+++ yt-api-service/functions/src/index.ts
@@ -12,6 +12,8 @@ import * as functions from "firebase-functions/v1";
 import {initializeApp} from "firebase-admin/app";
 import {Firestore} from "firebase-admin/firestore";
 import * as logger from "firebase-functions/logger";
+import {Storage} from "@google-cloud/storage";
+import {onCall} from "firebase-functions/v2/https";
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
@@ -19,6 +21,9 @@ import * as logger from "firebase-functions/logger";
 initializeApp();
 
 const firestore = new Firestore();
+const storage = new Storage();
+
+const rawVideoBucketName = "vtube-raw-videos";
 
 export const createUser = functions.auth.user().onCreate((user) => {
   const userInfo = {
@@ -31,3 +36,30 @@ export const createUser = functions.auth.user().onCreate((user) => {
   logger.info(`User Created: ${JSON.stringify(userInfo)}`);
   return;
 });
+
+
+export const generateUploadUrl = onCall(
+  {maxInstances: 1}, async (request) => {
+    // Check if the user is authenticated
+    if (!request.auth) {
+      throw new functions.https.HttpsError(
+        "unauthenticated",
+        "You must be authenticated to upload a video."
+      );
+    }
+
+    const auth = request.auth;
+    const data = request.data;
+    const bucket = storage.bucket(rawVideoBucketName);
+
+    // Generate a unique filename
+    const fileName = `${auth.uid}-${Date.now()}.${data.fileExtension}`;
+    // Get a v4 signed URL for uploading file
+    const [url] = await bucket.file(fileName).getSignedUrl({
+      version: "v4",
+      action: "write",
+      expires: Date.now() + 15 * 60 * 1000, // 15 minutes
+    });
+
+    return {url, fileName};
+  });
