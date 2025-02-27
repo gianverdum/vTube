92bf9e1041d7beac3094490e38047caade081d87
gianverdum <gian.verdum@gmail.com>
2025-02-27 11:40:30 -0300

    Added upload video feature to web app

--- yt-web-client/app/navbar/navbar.tsx
+++ yt-web-client/app/navbar/navbar.tsx
@@ -7,6 +7,7 @@ import SignIn from './sign-in';
 import { onAuthStateChangedHelper } from '../firebase/firebase';
 import { useEffect, useState } from 'react';
 import { User } from 'firebase/auth';
+import Upload from './upload';
 
 export default function Navbar() {
     // Init user state
@@ -28,7 +29,7 @@ export default function Navbar() {
                     src="/youtube-logo.svg" alt="YouTube Logo" />
             </Link>
             {
-                // TODO: Add upload button
+                user && <Upload />
             }
             <SignIn user={user} />
         </nav>
